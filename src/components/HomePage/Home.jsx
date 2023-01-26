46996909a8e5a5d556c61bce6c462be40c352969
Tigran <gevorgyant05@gmail.com>
2023-01-26 15:25:56 +0400

    added navbar component and corrected router

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -2,12 +2,12 @@ import { createUseStyles } from 'react-jss';
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import FullScreenDialog from '../Search/SearchDialog';
-import PrimaryButton from '../Button/Button';
 import SignUpDialog from '../SignUp/SignUpDialog';
 import SignInDialog from '../SignIn/SignIn';
-import ProfileIcon from '../ProfileIcon/ProfileIcon';
 import MenuBar from '../Menu/MenuBar';
 import CarouselBox from '../CarouselBox';
+import Navbar from '../Navbar/Navbar';
+
 const useStyles = createUseStyles({
 	header: {
 		backgroundColor: '#3a3330',
@@ -40,89 +40,25 @@ const useStyles = createUseStyles({
 	headerTopLeft: { marginLeft: 15 },
 });
 
-function Home({ setHideHome }) {
+function Home({
+	setIsOpenMenu,
+	isOpenMenu,
+	openHome,
+	signInDialogOpen,
+	handleSignInClose,
+	handleSignInClickOpen,
+	signUpDialogOpen,
+	handleSignUpClose,
+	handleSearchClickOpen,
+	handleSearchClose,
+	handleSignUpClickOpen,
+	searchDialogOpen,
+}) {
 	const classes = useStyles();
-
-	const [searchDialogOpen, setSearchDialogOpen] = useState(false);
-	const [signUpDialogOpen, setSignUpDialogOpen] = useState(false);
-	const [signInDialogOpen, setSignInDialogOpen] = useState(false);
-	const [isOpenMenu, setIsOpenMenu] = useState(false);
-	const [openHome, setOpenHome] = useState(true);
-	const handleSignUpClickOpen = () => {
-		setSignUpDialogOpen(true);
-	};
-	const handleSignUpClose = () => {
-		setSignUpDialogOpen(false);
-	};
-	const handleSearchClickOpen = () => {
-		setSearchDialogOpen(true);
-	};
-
-	const handleSearchClose = () => {
-		setSearchDialogOpen(false);
-	};
-	const handleSignInClickOpen = () => {
-		setSignInDialogOpen(true);
-	};
-	const handleSignInClose = () => {
-		setSignInDialogOpen(false);
-	};
-
-	const handelClickMenuBar = () => {
-		setIsOpenMenu(!isOpenMenu);
-	};
 
 	return (
 		<>
 			<div className={classes.app}>
-				<div className={classes.header}>
-					<div className={classes.headerTopLeft}>
-						<img
-							src="/img/menu.png"
-							width={23}
-							height={23}
-							onClick={handelClickMenuBar}
-							style={{ cursor: 'pointer' }}
-						/>
-					</div>
-					<div className={classes.name}>
-						<Link to="/">
-							<img
-								src="/img/bork.jpg"
-								onClick={() => {
-									setOpenHome(true);
-								}}
-								width={100}
-								height={25}
-							/>
-						</Link>
-					</div>
-					<div className={classes.headerTopRight}>
-						<ProfileIcon setHideHome={setHideHome} setOpenHome={setOpenHome} />
-						<PrimaryButton
-							onClick={handleSignInClickOpen}
-							disabled={false}
-							variant="text"
-						>
-							sign in
-						</PrimaryButton>
-						<PrimaryButton
-							onClick={handleSignUpClickOpen}
-							disabled={false}
-							variant="text"
-						>
-							sign up
-						</PrimaryButton>
-						<img src="/img/bag.png" width={23} height={23} />
-						<img
-							src="/img/search.png"
-							width={23}
-							height={23}
-							className={classes.searchIcon}
-							onClick={handleSearchClickOpen}
-						/>
-					</div>
-				</div>
 				<FullScreenDialog
 					handleClickOpen={handleSearchClickOpen}
 					handleClose={handleSearchClose}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,102 @@
+import { createUseStyles } from 'react-jss';
+import { Link, Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import PrimaryButton from '../Button/Button';
+import ProfileIcon from '../ProfileIcon/ProfileIcon';
+
+const useStyles = createUseStyles({
+	header: {
+		backgroundColor: '#3a3330',
+		width: '100%',
+		height: 60,
+		header: {},
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	name: {
+		marginLeft: '200px',
+		color: 'white',
+		fontSize: 27,
+	},
+	headerTopRight: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		width: 250,
+		marginRight: 12,
+	},
+
+	searchIcon: {
+		'&:hover': {
+			cursor: 'pointer',
+		},
+	},
+	headerTopLeft: { marginLeft: 15 },
+});
+
+function Navbar({
+	handelClickMenuBar,
+	setOpenHome,
+	handleSignInClickOpen,
+	handleSignUpClickOpen,
+	handleSearchClickOpen,
+}) {
+	const classes = useStyles();
+	return (
+		<>
+			<div className={classes.header}>
+				<div className={classes.headerTopLeft}>
+					<img
+						src="/img/menu.png"
+						width={23}
+						height={23}
+						onClick={handelClickMenuBar}
+						style={{ cursor: 'pointer' }}
+					/>
+				</div>
+				<div className={classes.name}>
+					<Link to="/">
+						<img
+							src="/img/bork.jpg"
+							onClick={() => {
+								setOpenHome(true);
+							}}
+							width={100}
+							height={25}
+						/>
+					</Link>
+				</div>
+				<div className={classes.headerTopRight}>
+					<ProfileIcon setOpenHome={setOpenHome} />
+					<PrimaryButton
+						onClick={handleSignInClickOpen}
+						disabled={false}
+						variant="text"
+					>
+						sign in
+					</PrimaryButton>
+					<PrimaryButton
+						onClick={handleSignUpClickOpen}
+						disabled={false}
+						variant="text"
+					>
+						sign up
+					</PrimaryButton>
+					<img src="/img/bag.png" width={23} height={23} />
+					<img
+						src="/img/search.png"
+						width={23}
+						height={23}
+						className={classes.searchIcon}
+						onClick={handleSearchClickOpen}
+					/>
+				</div>
+			</div>
+			<Outlet />
+		</>
+	);
+}
+
+export default Navbar;
