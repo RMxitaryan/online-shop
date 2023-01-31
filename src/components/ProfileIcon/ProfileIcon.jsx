cfc7542d9d71a1f49f2d398e0a6a56751b941056
RMxitaryan <romanmxitaryan12@gmail.com>
2023-01-31 23:03:11 +0300

    add cards from firebase and cards in redux

--- src/components/ProfileIcon/ProfileIcon.jsx
+++ src/components/ProfileIcon/ProfileIcon.jsx
@@ -1,120 +1,122 @@
-import * as React from 'react';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { createUseStyles } from 'react-jss';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { createUseStyles } from "react-jss";
+import { Link, useNavigate } from "react-router-dom";
 
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import { auth } from '../../config/Config';
-import { useDispatch } from 'react-redux';
-import { setUser } from '../../redux/user/actions';
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { auth } from "../../config/Config";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../redux/user/actions";
 const useStyles = createUseStyles({
-	profileIcone: {
-		cursor: 'pointer',
-		color: 'white',
-		height: 30,
-		width: 30,
-	},
-	link: {
-		textDecoration: 'none',
-		color: '#3a3333',
-		'&:hover': {
-			color: '#3a3333',
-		},
-	},
+  profileIcone: {
+    cursor: "pointer",
+    color: "white",
+    height: 30,
+    width: 30,
+  },
+  link: {
+    textDecoration: "none",
+    color: "#3a3333",
+    "&:hover": {
+      color: "#3a3333",
+    },
+  },
 });
 
 function ProfileIcon({ openHome, setOpenHome }) {
-	const classes = useStyles();
-	const [anchorEl, setAnchorEl] = React.useState(null);
-	const open = Boolean(anchorEl);
-	const dispatch = useDispatch();
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	//   auth.onAuthStateChanged((user) => {
-	//     console.log(user.email);
-	//     dispatch(setUser())
-	//   });
+  //   auth.onAuthStateChanged((user) => {
+  //     console.log(user.email);
+  //     dispatch(setUser())
+  //   });
 
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-	return (
-		<>
-			<AccountCircleOutlinedIcon
-				className={classes.profileIcone}
-				src="/img/user.png"
-				aria-controls={open ? 'demo-positioned-menu' : undefined}
-				aria-haspopup="true"
-				aria-expanded={open ? 'true' : undefined}
-				onClick={handleClick}
-			/>
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <AccountCircleOutlinedIcon
+        className={classes.profileIcone}
+        src="/img/user.png"
+        aria-controls={open ? "demo-positioned-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      />
 
-			<Menu
-				id="demo-positioned-menu"
-				aria-labelledby="demo-positioned-button"
-				anchorEl={anchorEl}
-				open={open}
-				onClose={handleClose}
-				anchorOrigin={{
-					vertical: 'top',
-					horizontal: 'left',
-				}}
-				transformOrigin={{
-					vertical: 'top',
-					horizontal: 'left',
-				}}
-			>
-				<Link to="/profile" className={classes.link}>
-					{
-						<MenuItem
-							onClick={() => {
-								handleClose();
-								setOpenHome(false);
-							}}
-						>
-							Profile
-						</MenuItem>
-					}
-				</Link>
-				<Link to="/favourite" className={classes.link}>
-					{
-						<MenuItem
-							onClick={() => {
-								handleClose();
-								setOpenHome(false);
-							}}
-						>
-							Favourite
-						</MenuItem>
-					}
-				</Link>
-				<Link to="/about" className={classes.link}>
-					{
-						<MenuItem
-							onClick={() => {
-								handleClose();
-								setOpenHome(false);
-							}}
-						>
-							About
-						</MenuItem>
-					}
-				</Link>
-				<MenuItem
-					onClick={() => {
-						auth.signOut();
-						dispatch(setUser({}));
-						setOpenHome(openHome);
-					}}
-				>
-					Log out
-				</MenuItem>
-			</Menu>
-		</>
-	);
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <Link to="signin/profile" className={classes.link}>
+          {
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                setOpenHome(false);
+              }}
+            >
+              Profile
+            </MenuItem>
+          }
+        </Link>
+        <Link to="/favourite" className={classes.link}>
+          {
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                setOpenHome(false);
+              }}
+            >
+              Favourite
+            </MenuItem>
+          }
+        </Link>
+        <Link to="/about" className={classes.link}>
+          {
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                setOpenHome(false);
+              }}
+            >
+              About
+            </MenuItem>
+          }
+        </Link>
+        <MenuItem
+          onClick={() => {
+            auth.signOut();
+            dispatch(setUser({}));
+            setOpenHome(openHome);
+            return navigate("/");
+          }}
+        >
+          Log out
+        </MenuItem>
+      </Menu>
+    </>
+  );
 }
 
 export default ProfileIcon;
--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,2 +1,3 @@
 export const SET_USER = "set_user";
 export const DELETE_USER = "delete_user";
+export const SET_CARD = "set_card";
